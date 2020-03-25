82bc3c84afd10d495e029052afc45e64e3b765db
checksonhk <jackson.to@gmail.com>
2020-03-25 19:05:18 -0400

    update logo

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a69fdea8d9be7df4e319342a88ad8a4.js"
+  "/precache-manifest.376686e4a7de84635fb41df5c2bd3d8e.js"
 );
 
 self.addEventListener('message', (event) => {
